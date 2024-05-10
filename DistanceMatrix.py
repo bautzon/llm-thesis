f2689f0beae983138406ed92bda44d537d7d144a
Theodor <theodorkier@protonmail.com>
2024-05-10 11:19:19 +0200

    correct labelling

--- DistanceMatrix.py
+++ DistanceMatrix.py
@@ -61,28 +61,11 @@ def calculate_variance_of_cosine_similarities(text, model):
 
 # * Process the data
 llama3_prompt1_json_data = read_json_file()
-llama3_prompt2_json_data = read_json_file('Test-Data/prompt_2_ai_output.json')
-prompt_2_human_answers_json_data = read_json_file('Test-Data/prompt_2_human_output.json')
 chatGpt3_prompt1_json_data = read_json_file('Test-Data/output_chatGpt_prompt1_Student.json')
+
+prompt_2_human_answers_json_data = read_json_file('Test-Data/prompt_2_human_output.json')
+llama3_prompt2_json_data = read_json_file('Test-Data/prompt_2_ai_output.json')
 chatGpt3_prompt2_json_data = read_json_file('Test-Data/output_chatGpt_prompt2_Student.json')
-
-# * Initialize Empty Lists
-hum_smoo_avg_list =          []
-human_vector_list =          []
-human_avg_len_list =         []
-human_cosine_list =          []
-human_distances =            []
-human_dist_variances =       []
-human_cosine_variances =     []
-
-syn_smoo_avg_list =          []
-synthetic_vector_list =      []
-synth_avg_len_list =         []
-synthetic_cosine_list =      []
-synthetic_distances =        []
-synthetic_dist_variances =   []
-synthetic_cosine_variances=  []
-
 
 def get_word_embedding(token, model):
     try:
@@ -150,86 +133,30 @@ def calculate_distance_and_more(creator, json_data):
     return avg_len_list, vector_list, cosine_list, cosine_variance_list, distances
 
 # * Calculate the distances and more
-human_avg_len_list, human_vector_list, human_cosine_list, human_cosine_variances, human_distances = calculate_distance_and_more('human', llama3_prompt1_json_data)
-synth_avg_len_list, synthetic_vector_list, synthetic_cosine_list, synthetic_cosine_variances, synthetic_distances = calculate_distance_and_more('ai', llama3_prompt1_json_data)
-gpt_prompt1_avg_len_list, gpt_prompt1_vector_list, gpt_prompt1_cosine_list, gpt_prompt1_cosine_variances, gpt_prompt1_distances = calculate_distance_and_more('ai', chatGpt3_prompt1_json_data)
+promp1_human_avg_len_list, prompt1_human_vector_list, prompt1_human_cosine_list, prompt1_human_cosine_variances, prompt1_human_distances = calculate_distance_and_more('human', llama3_prompt1_json_data)
+prompt1_llama3_avg_len_list, prompt1_llama3_vector_list, prompt1_llama3_cosine_list, prompt1_llama3_cosine_variances, prompt1_llama3_distances = calculate_distance_and_more('ai', llama3_prompt1_json_data)
+prompt1_gpt3_avg_len_list, prompt1_gpt3_vector_list, prompt1_gpt3_cosine_list, prompt1_gpt3_cosine_variances, prompt1_gpt3_distances = calculate_distance_and_more('ai', chatGpt3_prompt1_json_data)
 
-""" previous_word = None
-for answer in json_data:
-    if answer['creator'] == 'human':
-        answer_text = answer['answer']
-        words = answer_text.split()
-        vector_norms = calculate_vector_norms(words, model)
-        if vector_norms:
-            average_norm = np.mean(vector_norms)
-            human_avg_len_list.append(average_norm)
-        for word in words:
-            word_embedding = get_word_embedding(word, model)
-            if word_embedding is not None:
-                current_word = word
-                if previous_word != current_word:
-                    #Do something with the word embedding or token
-                    vector_distance = calculate_distance(current_word, previous_word)
-                    human_vector_list.append(vector_distance)
-                    if previous_word in model and current_word in model:
-                        sim = cosine_similarity(model[previous_word], model[current_word])
-                        human_cosine_list.append(sim) 
-                        human_variance = np.var(human_cosine_list)   
-                previous_word = current_word
-        human_cosine_variances.append(human_variance)
-    if answer['creator'] == 'ai':
-        answer_text = answer['answer']
-        words = answer_text.split()
-        vector_norms = calculate_vector_norms(words, model)
-        if vector_norms:
-            average_norm = np.mean(vector_norms)
-            synth_avg_len_list.append(average_norm)
-        for word in words:
-            word_embedding = get_word_embedding(word, model)
-            if word_embedding is not None:
-                current_word = word
-                if previous_word != current_word:
-                    #Do something with the word embedding or token
-                    vector_distance = calculate_distance(current_word, previous_word)
-                    synthetic_vector_list.append(vector_distance)   
-                    if previous_word in model and current_word in model:
-                        sim = cosine_similarity(model[previous_word], model[current_word])
-                        synthetic_cosine_list.append(sim)
-                        synthetic_cos_variance = np.var(synthetic_cosine_list)
-                previous_word = current_word
-        synthetic_cosine_variances.append(synthetic_cos_variance)
- """        
-            
-
-""" # * Should probably be introduced in the for-loop above
-for answer in llama3_prompt1_json_data:
-    if answer['creator'] == 'human':
-        dist = calculate_distances_for_text(answer['answer'], model)
-        if dist is not None:
-            human_distances.append(dist)
-    elif answer['creator'] == 'ai':
-        dist = calculate_distances_for_text(answer['answer'], model)
-        if dist is not None:
-            synthetic_distances.append(dist) """
-            
+prompt2_human_avg_len_list, prompt2_human_vector_list, prompt2_human_cosine_list, prompt2_human_cosine_variances, prompt2_human_distances = calculate_distance_and_more('human', prompt_2_human_answers_json_data)
+prompt2_llama3_avg_len_list, prompt2_llama3_vector_list, prompt2_llama3_cosine_list, prompt2_llama3_cosine_variances, prompt2_llama3_distances = calculate_distance_and_more('ai', llama3_prompt2_json_data)
+prompt2_gpt3_avg_len_list, prompt2_gpt3_vector_list, prompt2_gpt3_cosine_list, prompt2_gpt3_cosine_variances, prompt2_gpt3_distances = calculate_distance_and_more('ai', chatGpt3_prompt2_json_data)         
 
 # Calculate derivatives of the distance arrays
-human_derivatives = np.gradient(human_distances)
-synthetic_derivatives = np.gradient(synthetic_distances)
+prompt1_human_derivatives = np.gradient(prompt1_human_distances)
+prompt1_llama3_derivatives = np.gradient(prompt1_llama3_distances)
 
-hum_smoo_avg_list = smoothing_average(human_avg_len_list, 10)
-syn_smoo_avg_list = smoothing_average(synth_avg_len_list,10)
+prompt1_human_smoo_avg_list = smoothing_average(promp1_human_avg_len_list, 10)
+prompt1_llama3_smoo_avg_list = smoothing_average(prompt1_llama3_avg_len_list,10)
 
 
 # * Plotting the original distance data
-plt.figure(figsize=(24, 6))
-plt.subplot(1, 4, 1)
-plt.plot(human_distances, label='human Distances', color='b')
-plt.plot(synthetic_distances, label='synthetic Distances', color='r')
-# Plot for GPT3 Prompt 1... that fucks up the results!
-plt.plot(gpt_prompt1_distances, label='GPT3 Prompt 1 Distances', color='g')
-plt.plot(smoothing_average(human_distances,5), label='Human - Smoothed Average', linestyle='--', color='black')
-plt.plot(smoothing_average(synthetic_distances, 5), label='Synthetic - Smoothed Average', color='black')
+plt.figure(figsize=(20, 8))
+plt.subplot(2, 4, 1)
+plt.plot(prompt1_human_distances, label='human Distances', color='b')
+plt.plot(prompt1_llama3_distances, label='Llama3 Distances', color='r')
+plt.plot(prompt1_gpt3_distances, label='GPT3 Prompt 1 Distances', color='g')
+plt.plot(smoothing_average(prompt1_human_distances,5), label='Human - Smoothed Average', linestyle='--', color='black')
+plt.plot(smoothing_average(prompt1_llama3_distances, 5), label='Llama3 - Smoothed Average', color='black')
 plt.title('Average Euclidean Distances for Word Pairs')
 plt.xlabel('Number of Answers')
 plt.ylabel('Average Distance')
@@ -237,11 +164,11 @@ plt.legend()
 plt.grid(True)
 
 # *  Plotting the Cosines of the Embedded Tokens
-plt.subplot(1, 4, 2)
+plt.subplot(2, 4, 2)
 #plt.plot(human_derivatives, label='Derivative of Text 1 Distances', color='b')
 #plt.plot(synthetic_derivatives, label='Derivative of Text 2 Distances', color='r')
-plt.plot(smoothing_average(human_cosine_list, 2000), label='Human Cosine Similarities', color='b')
-plt.plot(smoothing_average(synthetic_cosine_list, 2000), label='Synthetic Cosine Similarities', color='r')
+plt.plot(smoothing_average(prompt1_human_cosine_list, 2000), label='Human Cosine Similarities', color='b')
+plt.plot(smoothing_average(prompt1_llama3_cosine_list, 2000), label='Llama3 Cosine Similarities', color='r')
 plt.title('Smothed Average of Cosine Similarity Scores')
 plt.xlabel('Number of Tokens')
 plt.ylabel('Cosine Similarity')
@@ -250,27 +177,39 @@ plt.grid(True)
 
 
 # *  Plotting the Variances
-plt.subplot(1, 4, 3)
-plt.plot(smoothing_average(human_cosine_variances, 1), label='Human Cosine Similarities', color='b')
-plt.plot(smoothing_average(synthetic_cosine_variances, 1), label='Synthetic Cosine Similarities', color='r')
+plt.subplot(2, 4, 3)
+plt.plot(smoothing_average(prompt1_human_cosine_variances, 1), label='Human Cosine Similarities', color='b')
+plt.plot(smoothing_average(prompt1_llama3_cosine_variances, 1), label='Llama3 Cosine Similarities', color='r')
 plt.title('Variance')
 plt.xlabel('Number of Answers')
-plt.ylabel('Average Squared difference between record and mean')
+plt.ylabel('Avg. Squared difference between record and mean')
 plt.legend()
 plt.grid(True)
 
 # * Plotting the vector distances
-plt.subplot(1, 4, 4)
-plt.plot(human_avg_len_list, label='Human', color='b')
-plt.plot(synth_avg_len_list, label='Synthetic', color='r')
-plt.plot(hum_smoo_avg_list, label='Human - Smoothed Average', linestyle='--', color='black')
-plt.plot(syn_smoo_avg_list, label='Synthetic - Smoothed Average', color='black')
+plt.subplot(2, 4, 4)
+plt.plot(promp1_human_avg_len_list, label='Human', color='b')
+plt.plot(prompt1_llama3_avg_len_list, label='Llama3', color='r')
+plt.plot(prompt1_human_smoo_avg_list, label='Human - Smoothed Average', linestyle='--', color='black')
+plt.plot(prompt1_llama3_smoo_avg_list, label='Llama3 - Smoothed Average', color='black')
 plt.title('Word Embedding Vector Distances')
 plt.xlabel('Word Index')
 plt.ylabel('Vector Distance')
 plt.legend()
 plt.grid(True)
 
+# Prompt 2
+plt.subplot(2, 4, 5)
+plt.plot(prompt2_llama3_distances, label='Llama3 Distances', color='r')
+plt.plot(prompt2_gpt3_distances, label='GPT3 Prompt 2 Distances', color='g')
+plt.plot(prompt2_human_distances, label='human Distances', color='b')
+plt.title('Average Euclidean Distances for Word Pairs')
+plt.xlabel('Number of Answers')
+plt.ylabel('Average Distance')
+plt.legend()
+plt.grid(True)
+
 plt.tight_layout()
 
 plt.show()
+
