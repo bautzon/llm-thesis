2f3acc94e58da5dfb34007de398893a193aecb8d
Theodor <theodorkier@protonmail.com>
2024-05-28 17:57:23 +0200

    gpt generator argument

--- data_processing/functions/chatGpt-generator.js
+++ data_processing/functions/chatGpt-generator.js
@@ -11,10 +11,10 @@ const prompt_2 = "Censorship in the Libraries. ‘All of us can think of a book 
 const prompt_2_human_like = "Answer the following and make it sound like it was written by a human. Censorship in the Libraries. ‘All of us can think of a book that we hope none of our children or any other children have taken off the shelf. But if I have the right to remove that book from the shelf -- that work I abhor -- then you also have exactly the same right and so does everyone else. And then we have no books left on the shelf for any of us.’ --Katherine Paterson, Author. Write a persuasive essay to a newspaper reflecting your views on censorship in libraries. Do you believe that certain materials, such as books, music, movies, magazines, etc., should be removed from the shelves if they are found offensive? Support your position with convincing arguments from your own experience, observations, and/or reading.";
 const prompt_2_student = "I am an American high school student that received the following question. Please answer it as a student would do. Censorship in the Libraries. ‘All of us can think of a book that we hope none of our children or any other children have taken off the shelf. But if I have the right to remove that book from the shelf -- that work I abhor -- then you also have exactly the same right and so does everyone else. And then we have no books left on the shelf for any of us.’ --Katherine Paterson, Author. Write a persuasive essay to a newspaper reflecting your views on censorship in libraries. Do you believe that certain materials, such as books, music, movies, magazines, etc., should be removed from the shelves if they are found offensive? Support your position with convincing arguments from your own experience, observations, and/or reading.";
 
-async function generate() {
+async function generate(question) {
   const completion = await openai.chat.completions.create({
     messages: [{ role: "system", content: "You are a helpful assistant." },
-        { role: "user", content: prompt_2_student}
+        { role: "user", content: question}
     ],
     model: "gpt-4",
   });
@@ -26,7 +26,7 @@ async function generate() {
 async function main() {
     let answers = {Answers: []};
     for (let i = 0; i < 10; i++) {
-        let res = await generate();
+        let res = await generate(prompt_2_student);
             answers.Answers.push({
                 id: i+1,
                 creator: "ai",
